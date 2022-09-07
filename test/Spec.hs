a83b49106dbf86081bd409d3292736e0c8e27025
Sandy Maguire <sandy@sandymaguire.me>
2022-09-06 22:33:59 -0700

    format: Spec

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,9 @@
 
 module Main where
 
+import Data.Foldable (traverse_)
 import Data.Int (Int8)
+import Data.Monoid (Any)
 import Data.Set (Set)
 import GHC.Generics (Generic)
 import Lasercutter
@@ -10,15 +12,52 @@ import Lasercutter.Types
 import Test.QuickCheck
 import Test.QuickCheck.Checkers hiding (Test)
 import Test.QuickCheck.Classes
-import Data.Foldable (traverse_)
-import Data.Monoid (Any)
 
 
-instance {-# OVERLAPPABLE #-} (CoArbitrary t, Arbitrary a, Arbitrary bc, CoArbitrary bc, CoArbitrary a) => Arbitrary (Parser bc t a) where
+type Test = Parser (Set Four) DebugTree
+
+
+main :: IO ()
+main = do
+  traverse_ quickCheck
+    [ -- expect is a left identity to optional
+      property $ expect . optional =-= id @(Test Int8)
+
+    , -- optional is a left identity to expect
+      property $ optional . expect =-= id @(Test (Maybe Int8))
+
+    , -- expect nothing is equivalent to fail
+      property $ expect (pure Nothing) =-= (Fail :: Test Int8)
+
+      -- expect distributes over liftA2
+    , property $ \(f :: Int8 -> Four -> Bool) a b ->
+        expect (liftA2 (liftA2 f) a b) =-= liftA2 @Test f (expect a) (expect b)
+
+      -- optional equivalent to try
+    , property $ optional @(Test) @Int8 =-= try
+    ]
+
+  quickBatch $ functor     $ undefined @_ @(Test (Int8, Int8, Int8))
+  quickBatch $ applicative $ undefined @_ @(Test (Int8, Int8, Int8))
+  quickBatch $ alternative $ undefined @_ @(Test Int8)
+  quickBatch $ semigroup   $ undefined @_ @(Test Any, Int8)
+  quickBatch $ monoid      $ undefined @_ @(Test Any)
+
+
+------------------------------------------------------------------------------
+
+instance {-# OVERLAPPABLE #-}
+    ( CoArbitrary t
+    , Arbitrary a
+    , Arbitrary bc
+    , CoArbitrary bc
+    , CoArbitrary a
+    ) =>
+    Arbitrary (Parser bc t a)
+      where
   arbitrary
     = let terminal
             = [ Pure <$> arbitrary
-              -- , pure GetCrumbs
               , Project <$> arbitrary
               , pure Fail
               ]
@@ -50,11 +89,19 @@ instance {-# OVERLAPPABLE #-} (CoArbitrary t, Arbitrary a, Arbitrary bc, CoArbit
   shrink Fail = []
 
 
-instance (CoArbitrary t, Arbitrary a, Arbitrary (Parser bc t a), CoArbitrary bc, CoArbitrary a, Arbitrary bc) => Arbitrary (Parser bc t [a]) where
+instance
+    ( CoArbitrary t
+    , Arbitrary a
+    , Arbitrary (Parser bc t a)
+    , CoArbitrary bc
+    , CoArbitrary a
+    , Arbitrary bc
+    ) =>
+    Arbitrary (Parser bc t [a])
+      where
   arbitrary
     = let terminal
             = [ Pure <$> arbitrary
-              -- , pure GetCrumbs
               , Project <$> arbitrary
               , pure Fail
               ]
@@ -78,9 +125,26 @@ instance (CoArbitrary t, Arbitrary a, Arbitrary (Parser bc t a), CoArbitrary bc,
               , OnChildren <$> scale (subtract 1) arbitrary
               , expect <$> scale (subtract 1) arbitrary
               ] <> terminal
+  shrink (Pure a)         = Fail : (fmap Pure $ shrink a)
+  shrink GetCrumbs        = [Fail]
+  shrink (LiftA2 _ _ _)   = [Fail]
+  shrink (Target _ pa')   = Fail : (pure $ fmap pure pa')
+  shrink (OnChildren pa') = Fail : (pure $ fmap pure pa')
+  shrink (Project _)      = [Fail]
+  shrink (Expect pa')     = Fail : (fmap expect $ shrink pa')
+  shrink Fail = []
 
 
-instance (CoArbitrary t, Arbitrary a, Arbitrary (Parser bc t a), CoArbitrary bc, CoArbitrary a, Arbitrary bc) => Arbitrary (Parser bc t (Maybe a)) where
+instance
+    ( CoArbitrary t
+    , Arbitrary a
+    , Arbitrary (Parser bc t a)
+    , CoArbitrary bc
+    , CoArbitrary a
+    , Arbitrary bc
+    ) =>
+    Arbitrary (Parser bc t (Maybe a))
+      where
   arbitrary
     = let terminal
             = [ Pure <$> arbitrary
@@ -106,7 +170,15 @@ instance (CoArbitrary t, Arbitrary a, Arbitrary (Parser bc t a), CoArbitrary bc,
               , try <$> scale (subtract 1) arbitrary
               , expect <$> scale (subtract 1) arbitrary
               ] <> terminal
+  shrink (Pure a)         = Fail : (fmap Pure $ shrink a)
+  shrink GetCrumbs        = [Fail]
+  shrink (LiftA2 _ _ _)   = [Fail]
+  shrink (Project _)      = [Fail]
+  shrink (Expect pa')     = Fail : (fmap expect $ shrink pa')
+  shrink Fail = []
 
+
+------------------------------------------------------------------------------
 
 data Four = A | B | C | D
   deriving (Eq, Ord, Show, Enum, Bounded, Generic)
@@ -115,6 +187,11 @@ instance Arbitrary Four where
   arbitrary = elements $ enumFromTo minBound maxBound
 
 instance CoArbitrary Four
+
+instance EqProp Four
+
+
+------------------------------------------------------------------------------
 
 data DebugTree
   = Leaf Int8
@@ -133,13 +210,14 @@ instance Arbitrary DebugTree where
 
 instance CoArbitrary DebugTree
 
+instance EqProp DebugTree
+
 instance IsTree DebugTree where
   getChildren (Leaf _) = []
   getChildren (Branch _ x) = x
 
-instance EqProp Four
 
-instance EqProp DebugTree
+------------------------------------------------------------------------------
 
 instance (EqProp a) => EqProp (Parser (Set Four) DebugTree a) where
   p1 =-= p2 = property $ do
@@ -148,36 +226,8 @@ instance (EqProp a) => EqProp (Parser (Set Four) DebugTree a) where
     pure $ runParser f t p1 =-= runParser f t p2
 
 
+------------------------------------------------------------------------------
+
 instance EqProp Int8 where
   (=-=) = (===)
-
-
-type Test = Parser (Set Four) DebugTree
-
-
-main :: IO ()
-main = do
-  traverse_ quickCheck
-    [ -- expect is a left identity to optional
-      property $ expect . optional =-= id @(Test Int8)
-
-    , -- optional is a left identity to expect
-      property $ optional . expect =-= id @(Test (Maybe Int8))
-
-    , -- expect nothing is equivalent to fail
-      property $ expect (pure Nothing) =-= (Fail :: Test Int8)
-
-      -- expect distributes over liftA2
-    , property $ \(f :: Int8 -> Four -> Bool) a b ->
-        expect (liftA2 (liftA2 f) a b) =-= liftA2 @Test f (expect a) (expect b)
-
-      -- optional equivalent to try
-    , property $ optional @(Test) @Int8 =-= try
-    ]
-
-  quickBatch $ functor     $ undefined @_ @(Test (Int8, Int8, Int8))
-  quickBatch $ applicative $ undefined @_ @(Test (Int8, Int8, Int8))
-  quickBatch $ alternative $ undefined @_ @(Test Int8)
-  quickBatch $ semigroup   $ undefined @_ @(Test Any, Int8)
-  quickBatch $ monoid      $ undefined @_ @(Test Any)
 
